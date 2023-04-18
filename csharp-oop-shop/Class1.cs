0a73a9c51840c7f5cf0331ce121912e7d0544592
stefanoreddd <127952037+stefanoreddd@users.noreply.github.com>
2023-04-18 18:17:55 +0200

    Add files via upload

---
+++ csharp-oop-shop/Class1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class Prodotto
+    {
+
+        // PROPRIETA'
+
+        private int codice;
+        public int CodiceProdotto { get { return this.codice; } }
+
+        private string nome;
+        public string NomeProdotto { get { return this.nome; } set { this.nome = value; } }
+
+        private string descrizione;
+        public string DescrizioneProdotto { get { return this.descrizione; } set { this.descrizione = value; } }
+
+        private int iva;
+        public int IvaProdotto { get { return this.iva; } set { this.iva = value; } }
+
+        private double prezzo;
+        public double PrezzoProdotto { get { return this.prezzo; } set { this.prezzo = value; } }
+
+        private double prezzoIva;
+        public double PrezzoProdottoConIva { get { return this.prezzoIva; } }
+
+
+
+        // COSTRUTTORE
+
+        public Prodotto(int codice, string nome, string descrizione, int iva, double prezzo)
+        {
+            this.codice = codice;
+
+            this.nome = nome;
+            this.descrizione = descrizione;
+            this.iva = iva;
+            this.prezzo = prezzo;
+
+        }
+
+        // GETTERS
+
+        public int GetCodiceProdotto()
+        {
+            return this.codice;
+        }
+
+        public string GetNomeProdotto()
+        {
+            return this.nome;
+        }
+
+        public string GetDescrizioneProdotto()
+        {
+            return this.descrizione;
+        }
+
+        public int GetIvaProdotto()
+        {
+            return this.iva;
+        }
+
+        public double GetPrezzoProdotto()
+        {
+            return (double)this.prezzo;
+        }
+
+
+
+
+        // SETTERS
+
+        public string SetNomeProdotto()
+        {
+            return this.nome;
+        }
+
+        public string SetDescrizioneProdotto()
+        {
+            return this.descrizione;
+        }
+
+        public int SetIvaProdotto()
+        {
+            return this.iva;
+        }
+
+        public double SetPrezzoProdotto()
+        {
+            return (double)this.prezzo;
+        }
+
+
+
+        // METODI
+
+        public int GeneraCodice()
+        {
+            Random rnd = new Random(8);
+            int num = rnd.Next();
+            codice = num;
+            Console.WriteLine("Codice Prodotto: " + codice);
+            return codice;
+        }
+
+
+        public double PrezzoConIva()
+        {
+            double prezzoIva = prezzo * 1.22;
+            return prezzoIva;
+        }
+
+        public void NomeEsteso()
+        {
+            Console.WriteLine("Nome Esteso: " + this.codice + ", " + this.nome);
+        }
+
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,13 @@
+﻿using Shop;
+
+Prodotto prodottoUser = new Prodotto(0, "Prodotto 1", "Prodotto dello Shop predefinito 1", 22, 58);
+
+Console.WriteLine("----- INFORMAZIONI PRODOTTO -----");
+prodottoUser.GeneraCodice();
+Console.WriteLine("Nome Prodotto: " + prodottoUser.GetNomeProdotto());
+prodottoUser.NomeEsteso();
+Console.WriteLine("Descrizione: " + prodottoUser.GetDescrizioneProdotto());
+Console.WriteLine("Prezzo: " + prodottoUser.GetPrezzoProdotto() + " euro");
+Console.WriteLine("Iva: " + prodottoUser.GetIvaProdotto() + "%");
+Console.WriteLine("Prezzo (con IVA): " + prodottoUser.PrezzoConIva() + " euro");
+Console.WriteLine("---------------------------------");
